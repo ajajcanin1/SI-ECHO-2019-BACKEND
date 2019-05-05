42e2f6678f96c4e085f59db95396000e7b89f6c6
Envera Husagic <ehusagic1@etf.unsa.ba>
2019-05-05 15:26:59 +0200

    Povezivanje sa bazom na backendu

--- src/main/java/com/example/echo/Repository/ZeljeniTerminRepository.java
+++ src/main/java/com/example/echo/Repository/ZeljeniTerminRepository.java
@@ -2,9 +2,14 @@ package com.example.echo.Repository;
 
 
 import com.example.echo.Entity.ZeljeniTermin;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
 
 
 public interface ZeljeniTerminRepository extends CrudRepository<ZeljeniTermin, Integer> {
-
+    //Hardkodirani id, jer nemamo podatke u prijavljenom profesoru
+    @Query("select zt from ZeljeniTermin zt where zt.predavac.id=null")
+    List<ZeljeniTermin> findZeljeniTerminByPredavacId (Integer predavacId);
 }
