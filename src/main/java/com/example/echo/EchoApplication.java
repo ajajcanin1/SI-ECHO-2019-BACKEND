85435c815cc014c9bcf405a3275076b044f6d982
ahabibovic1 <ahabibovic1@etf.unsa.ba>
2019-06-12 20:42:05 +0200

    Implementacija funkcije za pocetak izvrsavanja algoritma

--- src/main/java/com/example/echo/EchoApplication.java
+++ src/main/java/com/example/echo/EchoApplication.java
@@ -1,12 +1,18 @@
 package com.example.echo;
 
+import com.example.echo.ga.v2.*;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 
 @SpringBootApplication
 public class EchoApplication {
 
+
 	public static void main(String[] args) {
 		SpringApplication.run(EchoApplication.class, args);
+		Driver driver = new Driver();
+		driver.Start();
 	}
 }
